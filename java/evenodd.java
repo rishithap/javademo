3047389a238048b3540a91cde7770fe55e03195c
Rishi <rishi@gmail.com>
2021-05-28 10:13:45 +0530

    all files added

---
+++ java/evenodd.java
@@ -0,0 +1,11 @@
+class Evenodd
+{
+ public static void main(String args[]}
+ {
+  int n=6;
+  if(n%2==0)
+    System.out.println("n is even");
+  else
+    System.out.println("n is odd");
+  }
+}
---
+++ java/multiplication.java
@@ -0,0 +1,8 @@
+class Multiplication
+{
+ public static void main(String args[])
+ {
+  int a=4,b=3;
+  System.out.println("Product is; "+(a*b));
+ }
+}
